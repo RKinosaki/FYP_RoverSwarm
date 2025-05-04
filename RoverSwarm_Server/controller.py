648c5ab60b0d13b2720b7ec7c82d7405c929a24e
RKinosaki <ryusei.20213@gmail.com>
2025-05-04 12:16:59 +0100

    Added Server

---
+++ RoverSwarm_Server/controller.py
@@ -0,0 +1,26 @@
+import socket
+
+##create tcp socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+##address found from the ip seen on phone hotspot
+server_address = ('192.168.75.151', 10000)
+print('starting up on ', server_address)
+sock.bind(server_address)
+sock.listen(10)
+
+while True:
+    print('waiting for connection')
+    connection, client_address = sock.accept()
+    print('connection from', client_address)  
+    try:  
+        while True:
+                ##wasd bind is used for movement command that is sent to the rover
+                data = connection.recv(1024)
+                try:
+                    print(data)
+                    print(int(data.hex(), 16))
+                except ValueError as e:
+                     print(e)
+    finally:
+        connection.close()        
+
