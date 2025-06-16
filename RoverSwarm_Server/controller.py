fb8f3cd2965bf4a2aabcf9524c65aba664ff7e83
RKinosaki <ryusei.20213@gmail.com>
2025-06-16 14:11:28 +0100

    small fix

--- RoverSwarm_Server/controller.py
+++ RoverSwarm_Server/controller.py
@@ -24,7 +24,7 @@ except KeyboardInterrupt:
 
     dProc.showpointcloud(R[0], bound)
     allPoints = dProc.showFilteredPointCloud(R[0], bound)
-    dProc.createGrid(allPoints)
+    dProc.createGrid(bound, allPoints)
 
 # LinReg(obstx_L, obsty_L, obstx_R, obsty_R)
 # RANSAC(obstx_L, obsty_L, obstx_R, obsty_R)  
